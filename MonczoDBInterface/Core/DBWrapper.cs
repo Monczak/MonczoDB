54cc9f03bbdcfe7e3d52f8713d66d439a55c90be
Monczak <szymon.jedrzejczak.sj@gmail.com>
2020-04-22 14:57:26 +0200

    Add file load dialog

---
+++ MonczoDBInterface/Core/DBWrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonczoDB;
+
+namespace MonczoDBInterface.Core
+{
+    public class DBWrapper
+    {
+        public static Database db;
+
+        public delegate void OnDBLoadedDelegate();
+        public static event OnDBLoadedDelegate OnDBLoaded;
+
+        public static async Task LoadFromFile(string path)
+        {
+            Task<Database> loadTask;
+            try
+            {
+                loadTask = Database.DeserializeAsync(File.OpenRead(path));
+                await loadTask;
+                db = loadTask.Result;
+            }
+            catch (FileNotFoundException e)
+            {
+                return;
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+
+            OnDBLoaded?.Invoke();
+
+            return;
+        }
+    }
+}
--- MonczoDBInterface/MainWindow.xaml.cs
+++ MonczoDBInterface/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MonczoDB;
+using MonczoDBInterface.Core;
 
 namespace MonczoDBInterface
 {
@@ -23,6 +27,85 @@ namespace MonczoDBInterface
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void UpdateStatusText(string text)
+        {
+            StatusText.Text = text;
+        }
+
+        private async void FileNewBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void FileLoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = "MonczoDB Databases (*.dat)|*.dat|All Files|*.*"
+            };
+
+            string filePath = null;
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    filePath = fileDialog.FileName;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    break;
+            }
+
+            UpdateStatusText($"Loading {System.IO.Path.GetFileName(filePath)}...");
+            await DBWrapper.LoadFromFile(filePath);
+            UpdateStatusText("Ready");
+        }
+
+        private async void FileSaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void FileSaveAsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataInsertRecordBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataDeleteRecordBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataInsertColumnBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataDeleteColumnBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataSortAscendingBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataSortDescendingBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void DataFilterBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
